3c3d83bca9a032ccc2f47df796c5aee16f4a0bce
Werikson_Alves <werikson.alves@ufv.br>
2024-09-10 11:58:59 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+"""
+...............................................................................................
+Description
+    Operation mode:
+        Build:     Creates a new database and saves it in json format
+        Recognize: Load the database, create the classifier and classify the actions
+
+    Operation stage:
+        0 - Processes the image and analyzes the operator's hand
+        1 - Processes the image and analyzes the operator's body
+        2 - Reduces the dimensionality of the data
+        3 - Updates and save the database
+        4 - Performs classification from kMeans
+...............................................................................................
+""" 
+
+from modules import *
+from sklearn.neighbors import KNeighborsClassifier
+import mediapipe as mp
+import os
+import rospy
+from std_msgs.msg import Int32
+
+# Initialize the Gesture Recognition System
+database = {'F': [], 'I': [], 'L': [], 'P': [], 'T': []}
+file_name_build = f"Datasets/DataBase_(5-10)_16.json"
+files_name= ['Datasets/DataBase_(5-10)_G.json',
+            'Datasets/DataBase_(5-10)_H.json',
+            'Datasets/DataBase_(5-10)_L.json',
+            'Datasets/DataBase_(5-10)_M.json',
+            'Datasets/DataBase_(5-10)_T.json',
+            'Datasets/DataBase_(5-10)_1.json',
+            'Datasets/DataBase_(5-10)_2.json',
+            'Datasets/DataBase_(5-10)_3.json',
+            'Datasets/DataBase_(5-10)_4.json',
+            'Datasets/DataBase_(5-10)_5.json',
+            'Datasets/DataBase_(5-10)_6.json',
+            'Datasets/DataBase_(5-10)_7.json',
+            'Datasets/DataBase_(5-10)_8.json',
+            'Datasets/DataBase_(5-10)_9.json',
+            'Datasets/DataBase_(5-10)_10.json'
+            ]
+name_val=f"val99"
+
+dataset_mode = ModeFactory.create_mode('dataset', database=database, file_name_build=file_name_build)
+validate_mode = ModeFactory.create_mode('validate', files_name=files_name, database=database, name_val=name_val)
+real_time_mode = ModeFactory.create_mode('real_time', files_name=files_name, database=database)
+
+mode = real_time_mode
+
+# Initialize the Servo Position System
+num_servos = 0 # Number of servos in the system
+if num_servos != 0:
+    dir_rot = 1 #direction of rotation
+    rospy.init_node('RecognitionSystem', anonymous=True)
+    pub_hor_rot = rospy.Publisher('/EspSystem/hor_rot', Int32, queue_size=10)
+    pub_ver_rot = rospy.Publisher('/EspSystem/ver_rot', Int32, queue_size=10)
+else:
+    pub_hor_rot = None
+    pub_ver_rot = None
+    dir_rot = 0
+    
+SPS = ServoPositionSystem(num_servos, pub_hor_rot, pub_ver_rot, dir_rot)
+
+grs = GestureRecognitionSystem(
+        # config=InitializeConfig('http://192.168.209.199:81/stream'),
+        config=InitializeConfig(4,10),
+        operation=mode,
+        file_handler=FileHandler(),
+        current_folder=os.path.dirname(__file__),
+        data_processor=DataProcessor(), 
+        time_functions=TimeFunctions(), 
+        gesture_analyzer=GestureAnalyzer(),
+        tracking_processor=YoloProcessor('yolov8n-pose.pt'), 
+        feature=HolisticProcessor(
+            mp.solutions.hands.Hands(
+                static_image_mode=False, 
+                max_num_hands=1, 
+                model_complexity=1, 
+                min_detection_confidence=0.75, 
+                min_tracking_confidence=0.75
+                ),
+            mp.solutions.pose.Pose(
+                static_image_mode=False, 
+                model_complexity=1, 
+                smooth_landmarks=True, 
+                enable_segmentation=False, 
+                smooth_segmentation=True, 
+                min_detection_confidence=0.75, 
+                min_tracking_confidence=0.75
+                )
+            ),
+        classifier=KNN(
+            KNeighborsClassifier(
+                n_neighbors=mode.k, 
+                algorithm='auto', 
+                weights='uniform'
+                )
+            ),
+        sps=SPS
+        )
+
+try:
+    grs.run()
+finally:
+    grs.stop()
+
---
+++ modules/__init__.py
@@ -0,0 +1,25 @@
+from .auxiliary.DrawGraphics import DrawGraphics
+from .auxiliary.FileHandler import FileHandler
+from .auxiliary.TimeFunctions import TimeFunctions
+
+from .classifier.knn import KNN
+from .classifier.interfaces import InterfaceClassifier
+
+from .gesture.DataProcessor import DataProcessor
+from .gesture.FeatureExtractor import FeatureExtractor
+from .gesture.GestureAnalyzer import GestureAnalyzer
+
+from .pdi.YoloProcessor import  YoloProcessor
+from .pdi.HolisticProcessor import  HolisticProcessor
+from .pdi.interfaces import InterfaceTrack
+from .pdi.interfaces import InterfaceFeature
+
+from .ros.ServoControl import CommunicationEspCam
+
+from .system.GestureRecognitionSystem import GestureRecognitionSystem
+from .system.ServoPositionSystem import ServoPositionSystem
+from .system.SystemSettings import InitializeConfig
+from .system.SystemSettings import ModeFactory
+from .system.SystemSettings import ModeDataset
+from .system.SystemSettings import ModeValidate
+from .system.SystemSettings import ModeRealTime
