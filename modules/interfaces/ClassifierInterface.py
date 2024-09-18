df3d7e976bea4db49b2418ad62c23f2c45301e1d
Werikson_Alves <werikson.alves@ufv.br>
2024-09-18 10:50:45 -0300

    Refactor ClassifierInterface to include type hints and method documentation

--- modules/interfaces/ClassifierInterface.py
+++ modules/interfaces/ClassifierInterface.py
@@ -1,14 +1,42 @@
 from abc import ABC, abstractmethod
+from typing import Any
+
 
 class ClassifierInterface(ABC):
+    """
+    An abstract base class that defines the interface for classifiers.
+    Subclasses must implement the 'fit', 'predict', and 'validate' methods.
+    """
+
     @abstractmethod
-    def fit(self, *args, **kwargs):
+    def fit(self, *args: Any, **kwargs: Any) -> None:
+        """
+        Train the classifier using the provided training data.
+        
+        :param args: Positional arguments for fitting the classifier.
+        :param kwargs: Keyword arguments for fitting the classifier.
+        :return: None
+        """
         pass
 
     @abstractmethod
-    def predict(self, *args, **kwargs):
+    def predict(self, *args: Any, **kwargs: Any) -> Any:
+        """
+        Predict the class label(s) for given input data.
+        
+        :param args: Positional arguments for prediction.
+        :param kwargs: Keyword arguments for prediction.
+        :return: Predicted class label(s).
+        """
         pass
 
     @abstractmethod
-    def validate(self, *args, **kwargs):
+    def validate(self, *args: Any, **kwargs: Any) -> Any:
+        """
+        Validate the classifier using the provided validation data.
+        
+        :param args: Positional arguments for validation.
+        :param kwargs: Keyword arguments for validation.
+        :return: Validation results (could vary depending on implementation).
+        """
         pass
