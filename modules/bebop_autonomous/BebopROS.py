6a9a38512b2aaf565d2666ff67ba7d497fc7a068
Werikson_Alves <werikson.alves@ufv.br>
2024-09-28 16:19:20 -0300

    Refactor BebopROS.py to update module imports and dependencies

--- modules/bebop_autonomous/BebopROS.py
+++ modules/bebop_autonomous/BebopROS.py
@@ -9,13 +9,18 @@ class BebopROS:
         self.file_path = os.path.join(os.path.dirname(__file__), 'images')
         self.drone_type = 'bebop2'
 
-        if not os.path.exists(self.file_path): os.makedirs(self.file_path)
+        if not os.path.exists(self.file_path):
+            os.makedirs(self.file_path)
 
     def VideoCapture(self):
         try:
             self.camera = DroneCamera(self.file_path)
-            self.camera.initialize_publishers(['camera_control', 'snapshot', 'set_exposure'])
-            self.camera.initialize_subscribers(['compressed'])
+            self.camera.initialize_publishers(['camera_control',
+                                               'snapshot',
+                                               'set_exposure']
+                                              )
+            self.camera.initialize_subscribers(['compressed'
+                                                ])
             self.camera.success_flags["isOpened"] = True
         except Exception as e:
             print(f"Error: {e}")
